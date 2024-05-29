20d7155e59cbafb39e4587de83da5c4bc8fb80b3
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-05-29 09:52:35 +0200

    Replace BouncyCastleFipsProvider with Folio-tls-utils
    The BouncyCastleFipsProvider library has been removed and replaced by the Folio-tls-utils library in the EdgeCoursesApplication class and pom.xml file. The commits also include logging the FIPS checks results into the application's main method.

--- src/main/java/org/folio/edge/courses/EdgeCoursesApplication.java
+++ src/main/java/org/folio/edge/courses/EdgeCoursesApplication.java
@@ -1,20 +1,21 @@
 package org.folio.edge.courses;
 
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import lombok.extern.log4j.Log4j2;
 import org.folio.spring.cql.JpaCqlConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.security.Security;
+import static org.folio.common.utils.tls.FipsChecker.getFipsChecksResultString;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JpaCqlConfiguration.class})
 @EnableFeignClients
+@Log4j2
 public class EdgeCoursesApplication {
 
   public static void main(String[] args) {
-    Security.addProvider(new BouncyCastleFipsProvider());
+    log.info(getFipsChecksResultString());
     SpringApplication.run(EdgeCoursesApplication.class, args);
   }
 
